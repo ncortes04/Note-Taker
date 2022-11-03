b742e0d68304b29dbe6a727e3e62d437146b7efc
ncortes04 <nickapoleon@gmail.com>
2022-11-02 22:03:53 -0700

    added comments and fixed notes not refreshing

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,30 +1,32 @@
 const Router = require('express').Router();
-const path = require('path');
+//creates unique id for each note which can be accessed to deleted
 const { v4: uuidv4 } = require('uuid');
 
 const fs = require('fs');
 const dbDir = './db/db.json'
+// router function to handle requests
 Router.post('/notes', (req, res) => {
     
   console.info(`${req.method} request received to add a note`);
-  
+  //defining request body which consists of a title and text
     const { text, title} = req.body;
-  
+  //checking if text and title exists, if so create a new constant called create review
     if (text && title) {
       const createReview = {
         title,
         text,
         id: uuidv4(),
       };
-  
+  //reads the file wiht dbDir constant that directs to db.json
       fs.readFile( dbDir, 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
+          //converts data into an object
           const parsedReviews = JSON.parse(data);
-  
+          //pushes the newly created review into the object
           parsedReviews.push(createReview);
-  
+          // reqrites the file wiht the new object on the end
           fs.writeFile( dbDir ,JSON.stringify(parsedReviews, null, 4),
             (writeErr) =>
               writeErr
@@ -56,24 +58,30 @@ Router.post('/notes', (req, res) => {
     })
 });
 Router.delete('/notes/:id', (req, res) => {
+  //params for request id
+  const reqParams = req.params.id
+  //reads file
   fs.readFile( dbDir, 'utf8', (err, data) => {
-    const reqParams = req.params.id
     if(err) {
         console.error(err);
     } else {
+      //converts into object
         const parsed = JSON.parse(data);
-        const result = parsed.filter((note) => note.id !== reqParams);
-
+        //filters newly added note and adds to the array
+        let result = parsed.filter((note) => note.id !== reqParams);
+      // writes teh file with the new note removed
         fs.writeFile( dbDir, JSON.stringify(result), (err) => {
             if(err) {
               console.log(err)
             } else {
+            //logs what id was removed
             console.log(`Item ${reqParams} has been deleted`);
             console.log('Success!!')
           }
          })
-
         }
     });
+    //makes page refresh list of notes
+    res.send('Delete request');
   });
   module.exports = Router;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-
+//const that grab the files we created in routes folder
 const apiRoutes = require('./routes/apiRoutes/api');
 const htmlRoutes = require('./routes/htmlRoutes/html');
 const express = require('express')
-
+//the port we will be using
 const PORT = 3001;
 
 const app = express();
-
+//making static public so css and html can access eachother
 app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+//app.use is opening files that redirect apis as well as html which is defined in index.js
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
